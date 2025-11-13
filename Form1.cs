d07f9222e2b972b17430971bac3b88ca4a5ef1c4
DingDongReallyFast <jhpipboy@gmail.com>
2025-11-13 13:54:45 -0700

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,24 @@
+namespace FlashcardQuiz_GUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string filePath = openFileDialog1.FileName;
+                MessageBox.Show("You selected: " + filePath);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,80 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlashcardQuiz_GUI
+{
+    class Quiz
+    {
+        private List<Question> Questions { get; set; }
+        public int Score { get; }
+
+        public Quiz()
+        {
+            Questions = new List<Question>();
+        }
+
+        public Quiz(List<Question> questions)
+        {
+            Questions = questions;
+        }
+
+        public void AddQuestion(Question question)
+        {
+            Questions.Add(question);
+        }
+    }
+
+
+    class Question
+    {
+        public string QuestionText { get; set; }
+        public string[] AnswerArray { get; set; }
+        public int CorrectAnswerIndex { get; set; }
+
+        public Question(string questionText, string[] answerArray)
+        {
+            QuestionText = questionText;
+
+            // Assign Correct answer index based on where the star is located
+            int i = 0;
+            foreach (string answer in answerArray)
+            {
+                if (answer.StartsWith("*"))
+                {
+                    CorrectAnswerIndex = i;
+                    answerArray[i] = answer.Substring(1);
+                }
+                i++;
+            }
+            AnswerArray = answerArray;
+        }
+
+        public void DisplayAnswer()
+        {
+            Console.WriteLine($"Correct Answer: {AnswerArray[CorrectAnswerIndex]}");
+        }
+
+        public bool IsCorrect(int userAnswerIndex)
+        {
+            return userAnswerIndex == CorrectAnswerIndex;
+        }
+    }
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Form1());
+        }
+    }
+}
